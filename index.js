7059cfbc8e8e5dc405c156679594c0c915b2a1b7
Jeff Titak <jtitak06@yahoo.com>
2022-05-29 22:49:49 -0500

    development page added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const menuButton = document.querySelector('.menu-btn');
-const mobileOverlay = document.querySelector('.mobile-overlay');
-const mobileTabs = document.querySelector('.mobile-tabs');
-const closeButton = document.querySelector('.close-btn');
+const menuButton = document.querySelector(".menu-btn");
+const mobileOverlay = document.querySelector(".mobile-overlay");
+const mobileTabs = document.querySelector(".mobile-tabs");
+const closeButton = document.querySelector(".close-btn");
 
-menuButton.addEventListener('click', () => {
-    mobileTabs.classList.add('show');
-    mobileOverlay.classList.add('show');
-    menuButton.classList.add('hide');
-})
+menuButton.addEventListener("click", () => {
+  mobileTabs.classList.add("show");
+  mobileOverlay.classList.add("show");
+  menuButton.classList.add("hide");
+});
 
-closeButton.addEventListener('click', () => {
-    mobileTabs.classList.remove('show');
-    mobileOverlay.classList.remove('show');
-    menuButton.classList.remove('hide');
-})
+closeButton.addEventListener("click", () => {
+  mobileTabs.classList.remove("show");
+  mobileOverlay.classList.remove("show");
+  menuButton.classList.remove("hide");
+});
